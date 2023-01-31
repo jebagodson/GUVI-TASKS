27ec4942921721ca9fc376002c02e555c75907db
jebagodson <jebagodson006@gmail.com>
2023-01-31 12:40:49 +0530

    Day 1 Tasks

---
+++ test.js
@@ -0,0 +1,28 @@
+var res=[
+    {
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  
+    },
+    {
+      "id": 2,
+      "title": "qui est esse",
+    },
+    {
+      "id": 3,
+      "title": "qui est esse", 
+    },
+    {
+      "id": 4,
+      "title": "eum et est occaecati",
+    },
+    {
+      "id": 5,
+      "title": "nesciunt quas odio",
+      "body": "rep"
+      
+    }];
+  
+    for(var i=0;i<res.length;i++){ 
+        console.log("id"+res[i].id," "+"title"+res[i].title);
+         } 
