d5bf950561bd9117b21a08d8752bb54b994ebf81
jebagodson <jebagodson006@gmail.com>
2023-03-17 21:20:06 +0530

    React Task

---
+++ Any 3 API/Fruits API/script.js
@@ -0,0 +1,26 @@
+const apiUrl = 'https://dog.ceo/api/breeds/list/all';
+const verseRefInput = document.querySelector('#verseRefInput');
+const fetchVerseBtn = document.querySelector('#fetchVerseBtn');
+const verseContainer = document.querySelector('#verseContainer');
+
+async function fetchVerse() {
+    // Get the current value of the input field
+    const verseRef = verseRefInput.value;
+    
+    // Construct the API URL
+    const urlWithVerseRef = apiUrl + verseRef;
+    
+    // Fetch the data from the API
+    const response = await fetch(urlWithVerseRef);
+    const data = await response.json();
+
+        // Update the UI with the verse text and reference
+        verseContainer.innerHTML = `
+          <p class="lead">${data.text}</p>
+          <p class="text-end"><em>${data.reference}</em></p>
+        `;
+      
+      }
+  
+  // Add an event listener to the button that fetches the verse
+  fetchVerseBtn.addEventListener('click', fetchVerse);
